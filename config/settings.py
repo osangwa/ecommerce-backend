77489704e499a11c80954c36bb752b907377cd11
osangwa <osangwa@environment.gov.rw>
2025-11-29 23:28:25 +0200

    Add STATIC_ROOT for deployment

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Custom user model
